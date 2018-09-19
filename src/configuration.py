1a5dca748a072d2876b6820df2548b618f4208de
Mark Crick <fatdunky@gmail.com>
2018-09-19 22:31:00 +1000

    fixed up some config directory parsing

--- src/configuration.py
+++ src/configuration.py
@@ -3,7 +3,7 @@ Created on 19Feb.,2017
 
 @author: fatdunky
 '''
-import configparser, logging, os
+import logging, os
 from utilites.configuration_parser import ConfigurationParser
 
 class Configuration(ConfigurationParser):
@@ -17,42 +17,49 @@ class Configuration(ConfigurationParser):
     DIRECTORY_NAME_DATA='data_directory'
     DIRECTORY_NAME_LOG='logging_directory'
     DIRECTORY_NAME_CONFIG='configuration_data'
-    
+
     SECTION_NAME_FILE_NAME = "file_name"
     ITEM_FILE_NAME_DELIMITERS="delimiters"
-    
+
     SECTION_NAME_MEDIA_TYPES="media_types"
     SECTION_NAME_MEDIA_SUB_TYPES="media_sub_types"
     ITEM_MEDIA_TYPES_SUBTYPES="subtypes"
     ITEM_MEDIA_TYPES_EXTENSIONS="extensions"
     ITEM_MEDIA_TYPES_DESTINATION_DIR="destination_dir"
-    ITEM_MEDIA_TYPES_MODULE_NAME="module_name"   
-    ITEM_MEDIA_TYPES_MODULE_CLASS="class"   
-    ITEM_MEDIA_TYPES_KNOWN_NAMES_FILE="known_names_list"   
-    
+    ITEM_MEDIA_TYPES_MODULE_NAME="module_name"
+    ITEM_MEDIA_TYPES_MODULE_CLASS="class"
+    ITEM_MEDIA_TYPES_KNOWN_NAMES_FILE="known_names_list"
+
     SECTION_NAME_WORD_FILE_NAMES = "word_file_names"
     ITEM_WORD_FILE_DELIMITER = 'delimiters'
     ITEM_WORD_FILE_FILE_NAME = 'file'
     ITEM_WORD_FILE_CLASS = 'class'
     ITEM_WORD_MODULE_NAME='module_name'
-     
+
     SECTION_NAME_LOGGING = "logging"
     LOGGING_FORMAT='logging_format'
     LOGGING_DATE_FORMAT='logging_date_format'
     LOGGING_LEVEL='logging_level'
     LOGGING_CONSOLE_MODE='logging_console_mode'
     LOGGING_FILE='logging_file'
-    
-    
+    LOGGING_WHEN='logging_when'
+    LOGGING_INTERVAL='logging_interval'
+    LOGGING_BACKUP_COUNT='logging_backup_count'
+    LOGGING_ROTATE_MODE='logging_rotate_mode'
+    LOGGING_BYTES='logging_bytes'
+
+
     def __init__(self, config_directory, config_file, default_config_directory, default_config_file):
         '''
         Constructor
-        '''    
-        #self._config = ConfigParser.ConfigParser()
+        '''
+        #self.config = ConfigParser.ConfigParser()
         #get config sections
         super(Configuration, self).__init__(default_config_directory,default_config_file)
         self.set_config_directory(config_directory)
         self.read_config(config_file)
+
+        print ("config sections: {}".format(self.config.sections()))
         self.__config_file = dict(self.config.items(Configuration.SECTION_NAME_CONFIG_FILE))
         self.__directories = dict(self.config.items(Configuration.SECTION_NAME_DIRECTORIES))
         self.__file_name = dict(self.config.items(Configuration.SECTION_NAME_FILE_NAME))
@@ -60,75 +67,75 @@ class Configuration(ConfigurationParser):
         self.__word_file_names = dict(self.config.items(Configuration.SECTION_NAME_WORD_FILE_NAMES))
         self.__logging = dict(self.config.items(Configuration.SECTION_NAME_LOGGING))
         self.__media_sub_type_names = dict(self.config.items(Configuration.SECTION_NAME_MEDIA_TYPES))
-        
+
         #get variable config sections - word files
         self.__word_files = {}
         for word_file_name in list(self.__word_file_names.values()):
             self.__word_files[word_file_name] = self.config.items(word_file_name)
-        
+
         #get variable config sections - media types
         self.__media_types = {}
         self.__media_sub_types = {"":[]}
         for media_type_name in list(self.__media_type_names.values()):
-            self.__media_types[media_type_name] = self.config.items(media_type_name)  
+            self.__media_types[media_type_name] = self.config.items(media_type_name)
             sub_types = self.get_media_type_subtypes(media_type_name)
             sub_types_dict = {}
             for sub_media_type_name in sub_types:
                 if (sub_media_type_name and sub_media_type_name.strip()):
-                    sub_types_dict[sub_media_type_name] = self.config.items(sub_media_type_name)  
+                    sub_types_dict[sub_media_type_name] = self.config.items(sub_media_type_name)
             self.__media_sub_types[media_type_name] = sub_types_dict
-                
-            
+
+
     def get_config_file_delimiter(self):
         return self.__config_file[self.ITEM_CONFIG_DELIMITER]
-    
+
     def get_file_name_delimiters(self):
         return self.__file_name[self.ITEM_FILE_NAME_DELIMITERS]
-    
+
     def get_media_type_subtypes(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return_value = media_types_items[self.ITEM_MEDIA_TYPES_SUBTYPES]
             return return_value.split(self.get_config_file_delimiter())
-        else:        
+        else:
             return []
-    
+
     def get_media_type_extensions(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return_value = media_types_items[self.ITEM_MEDIA_TYPES_EXTENSIONS]
             return return_value.split(self.get_config_file_delimiter())
-        else:        
+        else:
             return []
 
     def get_media_type_destination_dir(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return media_types_items[self.ITEM_MEDIA_TYPES_DESTINATION_DIR]
-        else:        
+        else:
             return ""
-    
+
     def get_media_type_module_name(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return media_types_items[self.ITEM_MEDIA_TYPES_MODULE_NAME]
-        else:        
+        else:
             return ""
-    
+
     def get_media_type_module_class(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return media_types_items[self.ITEM_MEDIA_TYPES_MODULE_CLASS]
-        else:        
+        else:
             return ""
-    
+
     def get_media_type_known_names_list(self, media_type):
         if (media_type in self.__media_types):
             media_types_items = dict(self.__media_types[media_type])
             return media_types_items[self.ITEM_MEDIA_TYPES_KNOWN_NAMES_FILE]
-        else:        
+        else:
             return ""
-    
+
     def get_media_sub_type_extensions(self, media_type, media_sub_type):
         if (media_type in self.__media_sub_types):
             sub_types_dict = self.__media_sub_types[media_type]
@@ -138,7 +145,7 @@ class Configuration(ConfigurationParser):
                 return return_value.split(self.get_config_file_delimiter())
             else:
                 return []
-        else:        
+        else:
             return []
 
     def get_media_sub_type_destination_dir(self, media_type, media_sub_type):
@@ -149,9 +156,9 @@ class Configuration(ConfigurationParser):
                 return media_sub_types_items[self.ITEM_MEDIA_TYPES_DESTINATION_DIR]
             else:
                 return ""
-        else:        
+        else:
             return ""
-    
+
     def get_media_sub_type_module_name(self, media_type, media_sub_type):
         if (media_type in self.__media_sub_types):
             sub_types_dict = self.__media_sub_types[media_type]
@@ -161,9 +168,9 @@ class Configuration(ConfigurationParser):
                 return media_sub_types_items_dict[self.ITEM_MEDIA_TYPES_MODULE_NAME]
             else:
                 return ""
-        else:        
+        else:
             return ""
-    
+
     def get_media_sub_type_module_class(self, media_type, media_sub_type):
         if (media_type in self.__media_sub_types):
             sub_types_dict = self.__media_sub_types[media_type]
@@ -172,9 +179,9 @@ class Configuration(ConfigurationParser):
                 return media_sub_types_items[self.ITEM_MEDIA_TYPES_MODULE_CLASS]
             else:
                 return ""
-        else:        
+        else:
             return ""
-    
+
     def get_media_sub_type_known_names_list(self, media_type, media_sub_type):
         if (media_type in self.__media_sub_types):
             sub_types_dict = self.__media_sub_types[media_type]
@@ -183,69 +190,85 @@ class Configuration(ConfigurationParser):
                 return media_sub_types_items[self.ITEM_MEDIA_TYPES_KNOWN_NAMES_FILE]
             else:
                 return ""
-        else:        
+        else:
             return ""
-    
-    
+
+
     def get_word_file_delimiter(self, word_file_name):
         if (word_file_name in self.__word_files):
             word_file_items = dict(self.__word_files[word_file_name])
             return word_file_items[self.ITEM_WORD_FILE_DELIMITER]
-        else:        
-            return "" 
-        
+        else:
+            return ""
+
     def get_word_file_file_name(self, word_file_name):
         if (word_file_name in self.__word_files):
             word_file_items = dict(self.__word_files[word_file_name])
             return word_file_items[self.ITEM_WORD_FILE_FILE_NAME]
-        else:        
-            return "" 
-    
+        else:
+            return ""
+
     def get_word_absolute_file_file_name(self, word_file_name):
         if (word_file_name in self.__word_files):
             word_file_items = dict(self.__word_files[word_file_name])
             return_value = self.get_data_directory() + os.path.sep + word_file_items[self.ITEM_WORD_FILE_FILE_NAME]
             return return_value
-        else:        
-            return "" 
+        else:
+            return ""
 
     def get_word_file_file_class(self, word_file_name):
         if (word_file_name in self.__word_files):
             word_file_items = dict(self.__word_files[word_file_name])
             return word_file_items[self.ITEM_WORD_FILE_CLASS]
-        else:        
-            return "" 
-    
+        else:
+            return ""
+
     def get_word_file_module_name(self, word_file_name):
         if (word_file_name in self.__word_files):
             word_file_items = dict(self.__word_files[word_file_name])
             return word_file_items[self.ITEM_WORD_MODULE_NAME]
-        else:        
-            return "" 
+        else:
+            return ""
 
     def get_data_directory(self):
         return self.__directories[self.DIRECTORY_NAME_DATA]
-    
+
     def get_log_directory(self):
         return self.__directories[self.DIRECTORY_NAME_LOG]
-    
-    def get_config_directory(self):
+
+    def get_config_data_directory(self):
         return self.__directories[self.DIRECTORY_NAME_CONFIG]
-    
+
     def get_log_format(self):
         return self.__logging[self.LOGGING_FORMAT]
-    
+
     def get_log_date_format(self):
         return self.__logging[self.LOGGING_DATE_FORMAT]
-    
+
     def get_log_level(self):
         return self.__logging[self.LOGGING_LEVEL]
-    
+
     def get_log_console_mode(self):
         return self.__logging[self.LOGGING_CONSOLE_MODE]
-    
+
     def get_log_file(self):
         return self.__logging[self.LOGGING_FILE]
+
+
+    def get_log_rotate_mode(self):
+        try:
+            return self.__logging[self.LOGGING_ROTATE_MODE]
+        except KeyError:
+            logging.info("No configuration item named %s found", self.LOGGING_ROTATE_MODE)
+            return None
+
+
+    def get_log_rotate_bytes(self):
+        try:
+            return self.__logging[self.LOGGING_BYTES]
+        except KeyError:
+            logging.info("No configuration item named %s found", self.LOGGING_BYTES)
+            return None
 
     def get_config_file(self):
         return self.__config_file
@@ -325,5 +348,4 @@ class Configuration(ConfigurationParser):
     word_file_names = property(get_word_file_names, set_word_file_names, del_word_file_names, "get dictionary of word_file_names's section from configuration file")
     logging = property(get_logging, set_logging, del_logging, "get dictionary of logging section from configuration file")
 
-    
-    
+
--- src/monkeyshifter.py
+++ src/monkeyshifter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 '''
 Created on 3Dec.,2016
@@ -8,7 +8,7 @@ Created on 3Dec.,2016
 
 import os, sys, getopt, logging, random
 from utilites.logger import Logger
-from configuration import Configuration 
+from configuration import Configuration
 from cmd_line.word_category_cmd import WordCategoryCmd
 from utilites.io import *
 from lines.line import Line
@@ -16,6 +16,22 @@ from mediatypes.media import Media
 from wordcategories.word_category import WordCategory
 from wordcategories.unmatched_word_category import UnmatchedWordCategory
 
+
+DEFAULT_CONFIG_DIR='config'
+DEFAULT_CONFIG_FILE='monkey_shifter.cfg'
+
+DEFAULT_LOGGER_FORMAT='%(asctime)s|%(name)s|%(levelname)s|%(message)s'
+DEFAULT_LOGGER_DATE_FORMAT='%Y%m%d|%H:%M:%S'
+DEFAULT_LOG_DIR='logs'
+DEFAULT_LOG_FILE='monkey_shifter.log'
+DEFAULT_LOG_LEVEL=logging.DEBUG
+
+DEFAULT_LOG_ROTATE_MODE='size'
+DEFAULT_LOG_ROTATE_WHEN='midnight'
+DEFAULT_LOG_ROTATE_INTERVAL=1
+DEFAULT_LOG_ROTATE_BACKUP_COUNT=30
+DEFAULT_CONSOLE_ONLY_MODE=False
+DEFAULT_LOG_ROTATE_BYTES=1000000
 
 def print_help():
     print('Usage: monkeyshifter.py [OPTION]... [TARGET DIRECTORY]...')
@@ -36,32 +52,64 @@ def print_help():
     print('-x, --console                             Log to console rather then log file')
     print('\n')
 
-def setup_configuration(config_directory,config_file):
+def setup_configuration(config_directory,config_file,default_config_dir,default_config_file):
     '''
     Setup configuration
     '''
-    return Configuration(config_directory,config_file)
+    return Configuration(config_directory,config_file,default_config_dir,default_config_file)
 
-def setup_logging(console_mode,log_file,log_directory, log_level):
+
+def setup_logging(console_mode,log_file,log_directory, log_level, rotate_mode, when, interval, backup_count, rotate_bytes):
     '''
     Setup logging
     '''
-    Logger.setLogDirectory(log_directory)
-    Logger.setLogFile(log_file)
-    Logger.setLoggingLevel(log_level)
-    Logger.setRotationFile(True, 'midnight', 1, 0)
-    Logger.updateHandler()
-    
+    if (console_mode == None or console_mode == ""):
+        console_mode = DEFAULT_CONSOLE_ONLY_MODE
+
+    if (log_file == None or log_file == ""):
+        log_file = DEFAULT_LOG_FILE
+
+    if (log_directory == None or log_directory == ""):
+        log_directory = DEFAULT_LOG_DIR
+
+    if (log_level == None or log_level == ""):
+        log_level = DEFAULT_LOG_LEVEL
+
+    if (rotate_mode == None or rotate_mode == ""):
+        rotate_mode = DEFAULT_LOG_ROTATE_MODE
+
+    if (when == None or when == ""):
+        when = DEFAULT_LOG_ROTATE_WHEN
+
+    if (interval == None or interval == ""):
+        interval = DEFAULT_LOG_ROTATE_INTERVAL
+
+    if (backup_count == None or backup_count == ""):
+        backup_count = DEFAULT_LOG_ROTATE_BACKUP_COUNT
+
+    if (rotate_bytes == None or rotate_bytes == ""):
+        rotate_bytes = DEFAULT_LOG_ROTATE_BYTES
+
+
+
+    Logger.set_log_directory(log_directory)
+    Logger.set_log_file(log_file)
+    Logger.set_logging_level(log_level)
+    #Logger.set_rotation_file(True, 'midnight', 1, 0)
+    Logger.set_rotation_file(DEFAULT_LOG_ROTATE_MODE, when, int(interval), int(backup_count), int(rotate_bytes))
+    Logger.update_handler()
+
+
 def create_media_object(line, delimeters, directory, test_mode):
     '''
     split line into words and create media object
     '''
     logging.info("Create Media Object")
-    
+
     unmatched_list = []
 
     retVal =  Media()
-    line_obj = Line() 
+    line_obj = Line()
     unmatched_list = line_obj.split_line(line, delimeters)
     logging.debug("Split line into %s",line_obj.get_words())
     retVal.set_line_obj(line_obj)
@@ -86,18 +134,18 @@ def create_media_object(line, delimeters, directory, test_mode):
     logging.debug("Got Size %s",retVal.get_size())
 
     logging.info("Finished Creating Media Object")
-    
+
     return unmatched_list, retVal
 
-def load_word_files(config, word_files):   
+def load_word_files(config, word_files):
     logging.info("Load word files")
     for word_file_name in list(config.get_word_file_names().values()):
         logging.debug("Loading %s",word_file_name)
-        class_string = "lines.wordcategories." + config.get_word_file_module_name(word_file_name) + "." + config.get_word_file_file_class(word_file_name)
+        class_string = "wordcategories." + config.get_word_file_module_name(word_file_name) + "." + config.get_word_file_file_class(word_file_name)
         logging.debug("Loading %s",word_file_name)
-        class_string = "lines.wordcategories." + config.get_word_file_module_name(word_file_name) + "." + config.get_word_file_file_class(word_file_name)
+        class_string = "wordcategories." + config.get_word_file_module_name(word_file_name) + "." + config.get_word_file_file_class(word_file_name)
         logging.debug("Loading class : %s",class_string)
-        word_class = Io.load_class(class_string)
+        word_class = load_class(class_string)
         word_class = word_class()
         logging.debug("Setting file name : %s in %s",config.get_word_absolute_file_file_name(word_file_name), config.get_word_file_file_class(word_file_name))
         word_class.set_file(config.get_word_absolute_file_file_name(word_file_name))
@@ -111,15 +159,15 @@ def load_word_files(config, word_files):
         word_class.load_file()
         word_files[word_file_name] = word_class
         WordCategory.wordCategories[word_file_name] = word_class
-        
+
     return word_files
 
 
 def load_media_type_class(config, media_type_name,media_types):
     logging.debug("Loading %s",media_type_name)
-    class_string = "lines.mediatypes." + config.get_media_type_module_name(media_type_name) + "." + config.get_media_type_module_class(media_type_name)
+    class_string = "mediatypes." + config.get_media_type_module_name(media_type_name) + "." + config.get_media_type_module_class(media_type_name)
     logging.debug("Loading class : %s",class_string)
-    media_type_class = Io.load_class(class_string)
+    media_type_class = load_class(class_string)
     media_type_class = media_type_class()
     logging.debug("Loading media type(%s) extensions %s",media_type_name,config.get_media_type_extensions(media_type_name))
     media_type_class.set_extensions(config.get_media_type_extensions(media_type_name))
@@ -128,14 +176,14 @@ def load_media_type_class(config, media_type_name,media_types):
     logging.debug("Loading media type(%s) known_names_list %s",media_type_name,config.get_media_type_known_names_list(media_type_name))
     media_type_class.set_known_names_file_name(config.get_media_type_known_names_list(media_type_name))
     media_types[media_type_name] = media_type_class
-    
+
     return media_types
 
 def load_media_sub_type_class(config, media_sub_type_name,media_type_name,media_types):
     logging.debug("Loading %s - %s",media_type_name,media_sub_type_name)
-    class_string = "lines.mediatypes." + config.get_media_sub_type_module_name(media_type_name,media_sub_type_name) + "." + config.get_media_sub_type_module_class(media_type_name,media_sub_type_name)
+    class_string = "mediatypes." + config.get_media_sub_type_module_name(media_type_name,media_sub_type_name) + "." + config.get_media_sub_type_module_class(media_type_name,media_sub_type_name)
     logging.debug("Loading subtype class : %s",class_string)
-    media_type_class = Io.load_class(class_string)
+    media_type_class = load_class(class_string)
     media_type_class = media_type_class()
     #logging.debug("Loading media type(%s - %s) extensions %s",media_type_name,media_sub_type_name,config.get_media_sub_type_extensions(media_type_name,media_sub_type_name))
     #media_type_class.set_extensions(config.get_media_sub_type_extensions(media_type_name,media_sub_type_name))
@@ -144,7 +192,7 @@ def load_media_sub_type_class(config, media_sub_type_name,media_type_name,media_
     logging.debug("Loading media sub type(%s - %s) known_names_list %s",media_type_name,media_sub_type_name,config.get_media_sub_type_known_names_list(media_type_name,media_sub_type_name))
     media_type_class.set_known_names_file_name(config.get_media_sub_type_known_names_list(media_type_name,media_sub_type_name))
     media_types[media_type_name] = media_type_class
-    
+
     return media_types
 
 def load_media_sub_types(config, media_type_name, media_sub_types):
@@ -161,59 +209,64 @@ def load_media_types(config, media_types):
         load_media_sub_types(config, media_type_name,media_types)
     return media_types
 
-def parse_file_names(file_names):
-    logging.info("Load media_types")
-    for line in names_to_parse:
-        (current_unmatched_word_list, media) = create_media_object(line,line_delimeters,target_directory,test_mode) 
+def parse_file_names(file_names, line_delimeters,target_directory,test_mode):
+    logging.info("Parse file names")
+    split_lines = []
+    unmatached_words_map = {}
+
+    for line in file_names:
+        (current_unmatched_word_list, media) = create_media_object(line,line_delimeters,target_directory,test_mode)
         split_lines.append(media)
         if (len(current_unmatched_word_list) > 0):
-            unmatched_words_map[line] = current_unmatched_word_list
-                
+            unmatached_words_map[line] = current_unmatched_word_list
+
     for line in split_lines:
         logging.debug("%s",line.get_line_obj().get_words())
 
+    return (split_lines, unmatached_words_map)
+
 
 def main(argv):
-    
+
     config_file = ''
     config_directory = ''
     data_directory = ''
-    
+
     tuning_mode = False
     verbose_mode = False
-    
-    base_dir = os.
-    
+
     log_file=''
     log_directory=''
     log_level= "DEBUG"
-    
+
     test_mode = False
     test_file = ''
     console_mode = False
     config = None
-    
+
     word_files = {}
     words = {}
-        
-    #We want a map of to module 
+
+    #We want a map of to module
     media_types = {}
-    
-    
+
+
     names_to_parse = []
     target_directory = ""
-    
+
     unmatched_words_map = {}
-    
+
     base_folder = os.path.abspath(os.path.dirname(__file__))
-    
-    DEFAULT_CONIF_DIR = os.path.abspath()
-    
-    
+
+    DEFAULT_CONFIG_DIR = os.path.join(base_folder, "../config")
+    DEFAULT_CONFIG_FILE = "monkey_shifter.cfg"
+
+    config_file = DEFAULT_CONFIG_FILE
+
     '''
     Handle command line arguments
     '''
-    
+
     try:
         opts, args = getopt.getopt(argv,"h?c:d:tvls:i:",["help","config_file=","config_directory=","tuning","debug","verbose","log_file=","log_level","log_directory=","test"])
     except getopt.GetoptError:
@@ -226,13 +279,13 @@ def main(argv):
         elif opt in ("-c", "--config_file"):
             config_file = arg
         elif opt in ("-d", "--config_directory"):
-            config_directory = arg    
+            config_directory = arg
         elif opt in ("-t"):
-            tuning_mode = True   
+            tuning_mode = True
         elif opt in ("-v", "--debug", "--verbose"):
-            verbose_mode = True 
+            verbose_mode = True
         elif opt in ("-l", "--log_file"):
-            log_file = arg 
+            log_file = arg
         elif opt in ("-e", "--log_level"):
             log_level = arg
         elif opt in ("-s", "--test"):
@@ -240,7 +293,7 @@ def main(argv):
             test_file = arg
         elif opt in ("-x", "--console"):
             console_mode = True
-    
+
     if (len(args) > 1):
         print_help()
         sys.exit(1)
@@ -252,55 +305,55 @@ def main(argv):
     else:
         logging.error("No directory specified. Using current directory")
         target_directory = os.getcwd()
-    
+
 
     config = setup_configuration(config_directory,config_file, DEFAULT_CONFIG_DIR, DEFAULT_CONFIG_FILE)
     if (config_directory == ""): config_directory = config.get_config_directory()
     if (data_directory == ""): data_directory = config.get_data_directory()
     if (log_directory == ""): log_directory = config.get_log_directory()
-    
+
     if (console_mode == False): console_mode = bool(config.get_log_console_mode())
     if (log_file == ""): log_file = config.get_log_file()
     if (log_level == ""): log_level = config.get_log_level()
-    setup_logging(console_mode,log_file,log_directory, log_level)
-    
+    setup_logging(console_mode,log_file,log_directory, log_level, base_folder)
+
     line_delimeters = config.get_file_name_delimiters()
-    
+
     #load word files
     word_files = load_word_files(config, word_files)
-    
+
     #load media types
     media_types = load_media_types(config, media_types)
-    
-    
-        
+
+
+
     if (test_mode and test_file != ""):
         logging.info("Load test list of file names from %s",test_file)
-       
-        names_to_parse = Io.read_file(test_file)
-        
+
+        names_to_parse = read_file(test_file)
+
         #Split each line into words
         logging.info("Found %i test file names",len(names_to_parse))
     else:
         #Load list of file names
-        
+
         logging.info("Load target list of file names in %s",target_directory)
         names_to_parse = os.listdir(target_directory)
-        
+
         #Split each line into words
         logging.info("Found %i file names",len(names_to_parse))
-    
-    split_lines = []
-    
-    
-      
+
+
+    (split_lines, unmatched_words_map) = parse_file_names(names_to_parse, line_delimeters, target_directory, test_mode)
+
+
     if (not tuning_mode):
         logging.info("Tuning mode enabled. Processing all unmatched words")
         inputCmd = WordCategoryCmd(unmatched_words_map)
         inputCmd.cmdloop()
-            
-    #logging.debug("done, newMatchedwords=[%s]",unmatched_words_map)       
-    
+
+    #logging.debug("done, newMatchedwords=[%s]",unmatched_words_map)
+
     for key, list in list(unmatched_words_map.items()):
         #logging.debug("Word word [%s], list [%s]", key, list)
         for word in list:
@@ -308,22 +361,22 @@ def main(argv):
             #word.get_category().add_new_word(word.get_text)
             #existing_list = word_files[word.get_category().get_config_name()]
             #existing_list.append(word.get_text)
-            
+
     logging.debug("%s", word_files)
     for word_category_name, word_category in list(word_files.items()):
         logging.debug("%s wordCat [%s]", word_category ,word_category_name)
 
-        
-        
-        
-    
+
+
+
+
     #Do tunning
     #add word diff score
-    
-    
-        
-    
-    
-            
+
+
+
+
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
